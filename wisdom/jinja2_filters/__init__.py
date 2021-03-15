c4f4bd387ed6b0a53d881b70ed29b1aa27c3fe12
Roman Adontz <roman@adontz.com>
2021-03-16 00:16:32 +0400

    Add HTML tag generating filters.

--- wisdom/jinja2_filters/__init__.py
+++ wisdom/jinja2_filters/__init__.py
@@ -4,18 +4,43 @@ from .relative_link import discover_relative_link
 from .relative_link import generate_relative_link
 from .subresource_integrity import discover_subresource_integrity
 from .subresource_integrity import generate_subresource_integrity
+from .tags import h1
+from .tags import h2
+from .tags import h3
+from .tags import h4
+from .tags import h5
+from .tags import h6
+from .tags import discover_a_page
+from .tags import generate_a_page
+
 
 JINJA2_DISCOVER_FILTERS = {
     'circled': discover_circled,
     'relative_link': discover_relative_link,
     'subresource_integrity': discover_subresource_integrity,
+    'h1': h1,
+    'h2': h2,
+    'h3': h3,
+    'h4': h4,
+    'h5': h5,
+    'h6': h6,
+    'a_page': discover_a_page,
 }
+
 
 JINJA2_GENERATE_FILTERS = {
     'circled': generate_circled,
     'relative_link': generate_relative_link,
     'subresource_integrity': generate_subresource_integrity,
+    'h1': h1,
+    'h2': h2,
+    'h3': h3,
+    'h4': h4,
+    'h5': h5,
+    'h6': h6,
+    'a_page': generate_a_page,
 }
+
 
 __all__ = (
     'JINJA2_DISCOVER_FILTERS',
